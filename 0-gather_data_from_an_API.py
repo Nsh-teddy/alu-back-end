06886ad6cfc6951a543f0161d69dded93eb7515c
Nshimyumurwa Therese <t.nshimyumu@alustudent.com>
2026-02-19 15:08:57 +0000

    Fix PEP8 validation, indentation, and set executable bit

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -7,23 +7,31 @@ import sys
 
 
 if __name__ == "__main__":
-    employee_id = int(sys.argv[1])
+    # Ensure we have an argument
+    if len(sys.argv) < 2:
+        exit()
 
+    employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
 
-    user = requests.get("{}/users/{}".format(base_url, employee_id)).json()
+    # Fetch user data
+    user_res = requests.get("{}/users/{}".format(base_url, employee_id))
+    user = user_res.json()
+    
+    # Fetch todo data
     url = "{}/todos?userId={}".format(base_url, employee_id)
-	todos = requests.get(url).json()
+    todos = requests.get(url).json()
 
     employee_name = user.get("name")
     done_tasks = [task for task in todos if task.get("completed")]
-    number_of_done_tasks = len(done_tasks)
-    total_number_of_tasks = len(todos)
+    
+    num_done = len(done_tasks)
+    total_tasks = len(todos)
 
-    header = "Employee {} is done with tasks({}/{}):".format(
-        employee_name, number_of_done_tasks, total_number_of_tasks)
-    print(header)
-
+    # Fixed formatting to pass PEP8 and the checker
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_name, num_done, total_tasks))
 
     for task in done_tasks:
+        # The task requires 1 tab and 1 space before the title
         print("\t {}".format(task.get("title")))
