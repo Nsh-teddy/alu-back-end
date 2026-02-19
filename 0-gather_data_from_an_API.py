3d8230ea9f0f19baa0fed4e89f2dc15b46f593d3
Nshimyumurwa Therese <t.nshimyumu@alustudent.com>
2026-02-19 15:13:54 +0000

    Final fix for PEP8 W503 and formatting

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-"""Script that, for a given employee ID, returns information about
-his/her TODO list progress using a REST API."""
-
+"""
+Script that, for a given employee ID, returns information about
+his/her TODO list progress using a REST API.
+"""
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    # Ensure we have an argument
+    # Get employee ID from the first command line argument
     if len(sys.argv) < 2:
         exit()
 
     employee_id = sys.argv[1]
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Fetch user data
-    user_res = requests.get("{}/users/{}".format(base_url, employee_id))
-    user = user_res.json()
-    
-    # Fetch todo data
-    url = "{}/todos?userId={}".format(base_url, employee_id)
-    todos = requests.get(url).json()
+    # 1. Fetch User data
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    user_res = requests.get(user_url)
+    user_data = user_res.json()
+    employee_name = user_data.get("name")
 
-    employee_name = user.get("name")
+    # 2. Fetch TODO list data
+    todo_url = "{}/todos?userId={}".format(base_url, employee_id)
+    todo_res = requests.get(todo_url)
+    todos = todo_res.json()
+
+    # 3. Process the tasks
     done_tasks = [task for task in todos if task.get("completed")]
-    
-    num_done = len(done_tasks)
     total_tasks = len(todos)
+    num_done = len(done_tasks)
 
-    # Fixed formatting to pass PEP8 and the checker
+    # 4. Print the header
+    # We keep the format call on one line or clean breaks to avoid W503
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_done, total_tasks))
 
+    # 5. Print the completed tasks with 1 tab and 1 space
     for task in done_tasks:
-        # The task requires 1 tab and 1 space before the title
         print("\t {}".format(task.get("title")))
